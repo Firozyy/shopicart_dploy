13d72c7cd405052894733af8e5ee6a2c47eb7588
Mohammed FIROS <mohammedfiros20@gmail.com>
2023-07-23 21:22:22 +0530

    serverlin1k

--- client/src/App.js
+++ client/src/App.js
@@ -1,45 +1,47 @@
-import React from 'react'
-import { BrowserRouter as Roter, Routes, Route } from "react-router-dom";
-import Home from './screen/Home';
-import Login from './screen/Login';
-import Register from './screen/Register';
-import Header from './compenets/Header';
-import Catagories from './compenets/Catagories';
-import TopNav from './compenets/TopNav';
-import Footer from './compenets/Footer';
-import ProductScreen from './screen/ProductScreen';
-import CreateProducrScreen from './screen/CreateProducrScreen';
-import Notfound from './screen/Notfound';
-import CartScreen from './screen/CartScreen';
-import ShippingScreen from './screen/ShippingScreen';
-import PaymentScreen from "./screen/PaymentScreen"
-import PlaceOrder from './screen/PlaceOrder';
-import OrderScreen from './screen/OrderScreen';
-import Success from './screen/Success';
+import express from "express"
+import dotenv from "dotenv"
+import { notFound, errorHandler } from "./midlewares/error.js";
+import path from "path"
+import {fileURLToPath} from 'url'
+dotenv.config({path:'./config/.env'});
 
-const App = () => {
-  return (
-    <Roter>
-      <TopNav />
-      <Header />
-      <Catagories />
-      <Routes>
-        <Route path='*' element={<Notfound />} />
-        <Route path='/' element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/product/:id' element={<ProductScreen />} exact />
-        <Route path='/cart/:id?' element={<CartScreen />} exact />
-        <Route path='/admin/createProduct' element={<CreateProducrScreen />} exact />
-        <Route path='/shipping' element={<ShippingScreen />} exact />
-        <Route path='/payment' element={<PaymentScreen />} exact />
-        <Route path='/placeOrder' element={<PlaceOrder />} exact />
-        <Route path='/order/:id' element={<OrderScreen />} exact />
-        <Route path='/paymentsuccess' element={<Success />} exact />
-      </Routes>
-      <Footer />
-    </Roter>
-  )
-}
+const app = express()
+app.use(express.json())
+app.use(express.urlencoded({
+    extended:true
+}));
+// import cors from 'cors'
+// app.use(cors({
+//     origin: process.env.Frontend_Url,
+//     credentials: true,
+//     methods: ["GET", "POST", "DELETE", "PUT",]
 
-export default App
+// }))
+
+
+
+
+import productRoute from "./Rout/productRoute.js"
+import userRoute from "./Rout/userRoutes.js"
+import orderRoute from "./Rout/orderRoutes.js"
+app.use("/api/v1", productRoute)
+app.use("/api/v1", userRoute)
+app.use("/api/v1", orderRoute)
+// build path
+const _filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(_filename)
+const buildPath = path.join(__dirname, '../client/build/');
+const indexPage = path.join(buildPath, '/index.html');
+
+app.use(express.static(buildPath));
+
+app.get('/*', (req,res)=>{
+    res.sendFile(indexPage);
+});
+
+app.use(notFound);
+app.use(errorHandler);
+
+
+export default app
+

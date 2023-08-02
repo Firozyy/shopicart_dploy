8c9a2ae27e927ae5d3c0435f77c8e2c714d747c1
Mohammed FIROS <mohammedfiros20@gmail.com>
2023-08-02 09:27:32 +0530

    build

--- backend/app.js
+++ backend/app.js
@@ -10,13 +10,13 @@ app.use(express.json())
 app.use(express.urlencoded({
     extended: true
 }));
-import cors from 'cors'
-app.use(cors({
-    origin: process.env.Frontend_Url,
-    credentials: true,
-    methods: ["GET", "POST", "DELETE", "PUT",]
+// import cors from 'cors'
+// app.use(cors({
+//     origin: process.env.Frontend_Url,
+//     credentials: true,
+//     methods: ["GET", "POST", "DELETE", "PUT",]
 
-}))
+// }))
 
 
 
@@ -28,16 +28,16 @@ app.use("/api/v1", productRoute)
 app.use("/api/v1", userRoute)
 app.use("/api/v1", orderRoute)
 // build path
-// const _filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(_filename)
-// const buildPath = path.join(__dirname, '../client/build/');
-// const indexPage = path.join(buildPath, '/index.html');
+const _filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(_filename)
+const buildPath = path.join(__dirname, '../client/build/');
+const indexPage = path.join(buildPath, '/index.html');
 
-// app.use(express.static(buildPath));
+app.use(express.static(buildPath));
 
-// app.get('/*', (req,res)=>{
-//     res.sendFile(indexPage);
-// });
+app.get('/*', (req,res)=>{
+    res.sendFile(indexPage);
+});
 
 app.use(notFound);
 app.use(errorHandler);
